71668439f9f559cf46be626dfb496dac7b167770
Paulo Márcio Matias da Silva <paulomarciomatiasdasilva@MacBook-Pro-de-Paulo.local>
2020-05-22 00:28:13 -0300

    Desafio finalizado e todos os testes aprovados.

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    if (uploadedFiles.length) {
+      const data = new FormData();
 
-    // TODO
+      uploadedFiles.forEach(file => {
+        data.append('file', file.file);
+      });
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
+      try {
+        await api.post('/transactions/import', data);
+        history.push('/');
+      } catch (err) {
+        console.log(err.response.error);
+      }
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const acceptedFiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(acceptedFiles);
   }
 
   return (
